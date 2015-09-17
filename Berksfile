c902758585346542d4f15eacd1b35315256d7c5f
popsikle <sal@appboy.com>
2015-09-17 00:31:00 -0400

    added berkshelf

---
+++ Berksfile
@@ -0,0 +1,5 @@
+source "https://supermarket.chef.io"
+
+metadata
+
+cookbook "opsit_libs", github: 'opsitters/opsit_libs'
--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ license          'Apache 2.0'
 description      'OpSitters recipes for some mundane tasks'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
+depends "opsit_libs"
+
 %w{
   apt
   chef_handler
